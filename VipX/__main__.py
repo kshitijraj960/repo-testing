b2dee2d1cefd8761d4ffa1e7f6963da5d6813bcc
kshitijraj960 <132722964+kshitijraj960@users.noreply.github.com>
2023-05-25 02:56:48 +0530

    Update __main__.py

--- VipX/__main__.py
+++ VipX/__main__.py
@@ -52,12 +52,12 @@ async def init():
     await userbot.start()
     await Vip.start()
     try:
-        await Vip.stream_decall("https://graph.org/file/59607e2988c84e571b7b0.jpg")
+        await Vip.stream_decall("https://telegra.ph/file/82f6a7d1d630c2f0106b5.jpg")
     except:
         pass
     try:
         await Vip.stream_call(
-            "https://graph.org/file/a4480da6e65dec33fa8ef.jpg"
+            "https://telegra.ph/file/82f6a7d1d630c2f0106b5.jpg"
         )
     except NoActiveGroupCall:
         LOGGER("VipX").error(
@@ -67,7 +67,7 @@ async def init():
     except:
         pass
     await Vip.decorators()
-    LOGGER("VipX").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗥𝗜𝗗𝗘𝗥♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    LOGGER("VipX").info("╔═════ஜ۩۞۩ஜ════╗\n  😈𝗠𝗔𝗗𝗘 𝗕𝗬 𝐌𝐀𝐅𝐈𝐀😈\n╚═════ஜ۩۞۩ஜ════╝")
     await idle()
 
 
