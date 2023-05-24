7690d4ea5ed5bc028c15c54565dc3344a7a06848
kshitijraj960 <132722964+kshitijraj960@users.noreply.github.com>
2023-05-25 02:24:19 +0530

    Update help.py

--- VipX/plugins/help.py
+++ VipX/plugins/help.py
@@ -88,7 +88,7 @@ async def helper_cb(client, CallbackQuery, _):
     if cb == "hb9":
         if CallbackQuery.from_user.id not in SUDOERS:
             return await CallbackQuery.answer(
-                   "😎𝗣𝗔𝗛𝗟𝗘 𓆩𝗥𝗜𝗗𝗘𝗥𓆪 𝗞𝗢 𝗣𝗔𝗣𝗔 𝗕𝗢𝗟 𝗝𝗔𝗞𝗘 😆😆", show_alert=True
+                   "😎ᴘᴀʜʟᴇ 𓆩ᴄʜᴀᴄʜᴀ𓆪 ᴋᴏ ᴘᴀᴘᴀ ʙᴏʟ ᴊᴀᴋᴇ 😆😆", show_alert=True
             )
         else:
             await CallbackQuery.edit_message_text(
